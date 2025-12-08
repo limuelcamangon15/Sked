0be6f7569fae08ed8a312e2ee7e7b043b65ff7db
Limuel <limuelmartincamangon@gmail.com>
2025-12-08 18:59:40 +0800

    finished landing page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between w-full h-fit px-50 py-4">
+    <header className="flex items-center justify-between w-full h-fit px-10 lg:px-40 py-4">
       <h1 className="text-white font-bold tracking-tighter text-3xl">Sked</h1>
 
       <div className="flex gap-5">
         <Link
           href={"/signup"}
-          className="py-2 px-4 cursor-pointer font-medium text-white/70 border border-white/20 rounded-lg hover:border-white transition-all duration-300"
+          className="py-2 px-4 cursor-pointer text-white/70 border border-white/40 rounded-lg hover:border-white hover:scale-103 transition-all duration-300"
         >
           Sign up
         </Link>
         <Link
           href={"/login"}
-          className="py-2 px-6 cursor-pointer font-medium text-black bg-white rounded-lg hover:bg-white/50 transition-all duration-300"
+          className="py-2 px-6 cursor-pointer text-white bg-blue-950 rounded-lg hover:bg-blue-900 hover:scale-103 transition-all duration-300"
         >
           Log in
         </Link>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,37 @@
+import {
+  ArrowLeft,
+  ArrowRightCircle,
+  LogIn,
+  Rocket,
+  UserPlus,
+} from "lucide-react";
+import Link from "next/link";
+
 function Hero() {
   return (
-    <div>
-      <h1>Sync your Events Schedules with Microsoft in one click</h1>
-    </div>
+    <section className="flex items-center justify-center px-8 md:px-16">
+      <div className="w-6/7">
+        <h1 className="text-5xl font-semibold tracking-tight leading-tight">
+          Smart Scheduling. Real-Time Microsoft Teams Sync. Powered by{" "}
+          <p className="inline tracking-tighter">Sked</p>.
+        </h1>
+
+        <p className="mt-6 text-lg text-neutral-600 max-w-lg">
+          Schedule your events in Sked and keep everything up to date in
+          Microsoft Teams.
+        </p>
+
+        <div className="mt-10 flex gap-4">
+          <Link
+            href={"/signup"}
+            className="flex items-center gap-2 group hover:scale-103 px-6 py-3 bg-blue-950 text-white rounded-xl shadow-lg hover:bg-blue-900 transition-all duration-300"
+          >
+            <Rocket className="group-hover:-rotate-360 transition-all duration-1000" />
+            Sign up and continue for free
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
 
